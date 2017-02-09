72497a368c95b34036bde18573aa092c7e41c768
GaizkaRubio <grubio@stratio.com>
2017-02-09 16:38:47 +0100

    First draft

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"time"
+	"net/http"
+	"log"
+	"encoding/csv"
+	"fmt"
+)
+
+func main() {
+	//marathonUrl := flag.String("marathon-url", "localhost:8080", "url where marathon API is exposed")
+	haproxyUrl := flag.String("haproxy-url", "gosec-int06.stratio.com:9090", "url where haproxy API is exposed")
+
+	flag.Parse()
+
+	ticker := time.NewTicker(time.Second * 2)
+
+	for range ticker.C {
+		resp, err := http.Get("http://" + *haproxyUrl + "/haproxy?stats;csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		r := csv.NewReader(resp.Body)
+
+		records, err := r.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tmp := make(map[string]string)
+
+		for i := range records {
+			tmp[i] = records[i]
+		}
+	}
+
+
+}
+
