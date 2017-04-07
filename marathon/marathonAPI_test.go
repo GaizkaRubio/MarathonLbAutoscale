30d4d3c884173fc21ac76ffea3334e0d6afe7368
GaizkaRubio <grubio@stratio.com>
2017-04-07 14:04:58 +0200

    new model structure

--- marathon/marathonAPI_test.go
+++ marathon/marathonAPI_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestGetApps(*testing.T){
 	apps := GetApps()
-	fmt.Println(apps.Apps[0])
-	SetAppInstances(apps.Apps[0], 3)
+	//fmt.Println(apps.Apps[0])
+	fmt.Println(apps.Apps[0].ID)
+ 	SetAppInstances(apps.Apps[0], 3)
 }
